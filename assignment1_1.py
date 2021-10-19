41550b83c921ef94556874ce19260070e47b3dd0
Carolmutua <gcarolmutua@gmail.com>
2021-10-19 17:33:50 +0300

    first assignment on git

---
+++ assignment1_1.py
@@ -0,0 +1,18 @@
+def main():
+    print("Hello World!")
+    print("Hello Again")
+    print('Yay!Printing.')
+    print("I'd much rather you 'not'.")
+    print('I "said" do not touch this.')
+
+main()
+
+    
+def main():
+    print("Hello World!")
+    print("Hello Again")
+    print('Yay!Printing.')
+    print("I'd much rather you 'not'.")
+    print('I "said" do not touch this.')
+
+main()
